daaddf870aa0dab788904e627f5bb30ffb309d5a
Nut <35489639+Nutcasee@users.noreply.github.com>
2023-06-06 15:53:44 +0700

    fuck sake, too late, running out of tiem, and...just doing nothing

--- ps2-tweets/test/twitter/ExtractTest.java
+++ ps2-tweets/test/twitter/ExtractTest.java
@@ -24,6 +24,7 @@ public class ExtractTest {
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
+//        what should i do? fuck sake..should.
     }
     
     @Test
